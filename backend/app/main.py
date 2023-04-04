d068f165dd18dbe73a424bf3ee1492548b72f45e
Ketov Daniil <ketov.daniil123@yandex.ru>
2023-04-04 14:16:25 +0300

    Initial commit

---
+++ backend/app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from app.config import db
+
+
+origins = ["http://localhost:3000"]
+
+
+def init_app():
+    db.init()
+
+    app = FastAPI(
+        title="buyhardware",
+        description="Buy hardware",
+        version="1"
+    )
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    @app.on_event("startup")
+    async def startup():
+        await db.create_all()
+
+    @app.on_event("shutdown")
+    async def shutdown():
+        await db.close()
+
+    return app
+
+
+app = init_app()
+
+
+def start():
+    """ Launched with 'poetry run start' at root level """
+    uvicorn.run("app.main:app", host="localhost", port=8888, reload=True)
